d70b54c22be9f636694b8b7db5c4d73d6097222d
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2020-07-17 06:25:03 +0800

    编解码及身份证出生年月日提取程序

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(191, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 30);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Code:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Encode:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Result:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {"Base64", "ASCII", "Unicode", "UTF-8"});
+            this.comboBox1.Location = new System.Drawing.Point(191, 143);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(226, 32);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(248, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Execute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(191, 262);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(226, 30);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(191, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(220, 26);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Encoding and Decoding";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(211, 376);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(176, 26);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Birthday extraction";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(271, 428);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(226, 30);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(178, 485);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 24);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Birthday:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(271, 482);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(226, 30);
+            this.textBox4.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(74, 478);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 38);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Extract";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label6.Location = new System.Drawing.Point(12, 431);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(243, 24);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Certificate of identification:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(978, 600);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] temp;
+            switch (comboBox1.Text)
+            {
+                case "Base64":
+                    temp = Encoding.UTF8.GetBytes(textBox1.Text);
+                    textBox2.Text = Convert.ToBase64String(temp);
+                    break;
+                case "ASCII":
+                    temp = Encoding.ASCII.GetBytes(textBox1.Text);
+                    textBox2.Text = "";
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        textBox2.Text = textBox2.Text + temp[i].ToString("X2");
+                    }
+
+                    break;
+                case "Unicode":
+                    temp = Encoding.Unicode.GetBytes(textBox1.Text);
+                    textBox2.Text = "";
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        textBox2.Text = textBox2.Text + temp[i].ToString("X2");
+                    }
+
+                    break;
+                case "UTF-8":
+                    temp = Encoding.UTF8.GetBytes(textBox1.Text);
+                    textBox2.Text = "";
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        textBox2.Text = textBox2.Text + temp[i].ToString("X2");
+                    }
+
+                    break;
+            }
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            this.ActiveControl = null;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            this.ActiveControl = null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text.Length.Equals(18) || textBox3.Text.Length.Equals(19))
+            {
+                string str = textBox3.Text.Substring(6, 8);
+                textBox4.Text = str.Substring(0, 4) + '/' + str.Substring(4, 2) + '/' + str.Substring(6, 2);
+            }
+            else
+            {
+                System.Windows.Forms.DialogResult dr = MessageBox.Show("输入错误", "", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
