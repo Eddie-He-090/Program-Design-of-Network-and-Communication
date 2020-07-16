54aa7faf8bacbd0533d39427a1b37a3b9d6d5be4
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2020-07-17 06:26:15 +0800

    长短按加减计数程序

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(626, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(460, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(918, 664);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            label1.Text = int.Parse(label1.Text) - 1 + ""; //label1的数字立即-1
+            timer1.Interval = 2000; //timer1_Tick那段代码每2秒执行一次，初始执行，在2秒之后才正式开始
+            timer1.Enabled = true; //开启timer1
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //无论用户在2s内还是在2s之后就松开鼠标，就马上停止timer1,timer2的代码执行
+            //如果用户在2s内松开鼠标,time3_Tick根本1次都不会执行
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer2.Interval = 50; //timer1的代码每0.05s执行一次
+            timer2.Enabled = true; //开始timer2
+            timer1.Enabled = false; //timer1_Tick这段代码就不要每2秒执行一次了，执行1次就好
+            //这句话就完成timer1与timer2的线程同步
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label1.Text = int.Parse(label1.Text) - 1 + "";
+        }
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+            label1.Text = int.Parse(label1.Text) + 1 + ""; //label1的数字立即+1
+            timer3.Interval = 2000; //timer3_Tick那段代码每2秒执行一次，初始执行，在2秒之后才正式开始
+            timer3.Enabled = true; //开启timer1        }
+        }
+
+        private void button2_MouseUp(object sender, MouseEventArgs e)
+        {
+            //无论用户在2s内还是在2s之后就松开鼠标，就马上停止timer1,timer2的代码执行
+            //如果用户在2s内松开鼠标,time3_Tick根本1次都不会执行
+            timer3.Enabled = false;
+            timer4.Enabled = false;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            timer4.Interval = 50; //timer4的代码每0.05s执行一次
+            timer4.Enabled = true; //开始timer4
+            timer3.Enabled = false; //timer3_Tick这段代码就不要每2秒执行一次了，执行1次就好
+            //这句话就完成timer3与timer4的线程同步
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            label1.Text = int.Parse(label1.Text) + 1 + "";
+        }
+    }
+}
